b9b06bd7237e9ff718e11e01bbb03ec910d320b0
shilpirana922 <ranashilpi922@gmail.com>
2020-11-02 13:19:43 +0530

    Updates

---
+++ precache-manifest.fa59dd6875e6742cca4de8682aa71c5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a3961e93106bf0ebe027ab2b3d241a",
+    "url": "/Event-Management-App/index.html"
+  },
+  {
+    "revision": "8f9c9882b8351e05e7c5",
+    "url": "/Event-Management-App/static/css/2.6bde65ae.chunk.css"
+  },
+  {
+    "revision": "71030bad9fc312cd634a",
+    "url": "/Event-Management-App/static/css/main.603a0310.chunk.css"
+  },
+  {
+    "revision": "8f9c9882b8351e05e7c5",
+    "url": "/Event-Management-App/static/js/2.babacbf4.chunk.js"
+  },
+  {
+    "revision": "77301219951e82722631bcc01796f6f6",
+    "url": "/Event-Management-App/static/js/2.babacbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71030bad9fc312cd634a",
+    "url": "/Event-Management-App/static/js/main.1f6d0f08.chunk.js"
+  },
+  {
+    "revision": "d9a446cb2e84928332a1",
+    "url": "/Event-Management-App/static/js/runtime-main.eabe684f.js"
+  }
+]);
